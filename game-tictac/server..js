0f9abbff46299f08c893d2631f9b1a60e8e81a22
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2023-05-07 01:00:30 +0530

    game tictac

---
+++ game-tictac/server..js
@@ -0,0 +1,187 @@
+let express=require("express")
+let app=express()
+let http=require("http")
+let socketio=require("socket.io")
+let server=http.createServer(app)
+let io=socketio(server)
+let{chat}=require("./chat")
+
+let matrix=[]
+for(let i=0;i<3;i++){
+    let arr=new Array(3).fill(null)
+    matrix.push(arr)
+} 
+
+let gamestatus={
+    player:[],
+    board:matrix,
+    nextplayer:"",
+    win:null,
+    winnerdata:[]
+   }
+
+
+io.on("connection",(socket)=>{
+    console.log("player connected to"+socket.id)
+
+    socket.on("users",(data)=>{
+        console.log(data)
+
+       
+        if(gamestatus.player.length<2){
+        gamestatus.player.push({
+            id:socket.id,
+            name:data.username,
+            room:data.room,
+            symbol:gamestatus.player[0] ?"o":"x"
+        })
+        }
+      
+        if(gamestatus.player.length==2 && gamestatus.player[0].room==gamestatus.player[1].room){
+            gamestatus.nextplayer=gamestatus.player[0].name
+            io.emit("gameBegins","gameBegins")
+        }
+    
+        io.emit("game",gamestatus)
+    }) 
+     
+    
+   
+    socket.on("clicked",(data)=>{
+        
+        //finding nextplayer
+        if(data[1]==gamestatus.nextplayer){
+           
+            let filter=gamestatus.player.filter((ele)=>{
+             return ele.name!=data[1]
+            })
+            console.log(filter,"filter")
+            gamestatus.nextplayer=filter[0].name
+              
+            }
+        
+        // adding the clied user to the gae board
+        if(data[0]=="cell-1"){
+            gamestatus.board[0][0]=data[1]
+        }
+        else if(data[0]=="cell-2"){
+            gamestatus.board[0][1]=data[1]
+        }
+        else if(data[0]=="cell-3"){
+            gamestatus.board[0][2]=data[1]
+        }
+        else if(data[0]=="cell-4"){
+            gamestatus.board[1][0]=data[1]
+        }
+        else if(data[0]=="cell-5"){
+            gamestatus.board[1][1]=data[1]
+        }
+        else if(data[0]=="cell-6"){
+            gamestatus.board[1][2]=data[1]
+        }
+        else if(data[0]=="cell-7"){
+            gamestatus.board[2][0]=data[1]
+        }
+        else if(data[0]=="cell-8"){
+            gamestatus.board[2][1]=data[1]
+        }
+        else if(data[0]=="cell-9"){
+            gamestatus.board[2][2]=data[1]
+        }
+        
+        // checking draw or not
+        function checkdraw(){
+            let count=0
+            for(let i=0;i<3;i++){
+                for(let j=0;j<3;j++){
+                    if(gamestatus.board[i][j]){
+                        count++
+                    }
+                }
+            }
+            if(count==9){
+                return true
+            }
+        }
+       //checking winner
+        function checkwin(){
+
+        for(let i=0;i<3;i++){
+            if((gamestatus.board[i][0]==gamestatus.board[i][1] && gamestatus.board[i][1]==gamestatus.board[i][2]) && gamestatus.board[i][0] !=null  && gamestatus.board[i][1] !=null && gamestatus.board[i][1] !=null){
+                return true
+            }
+        }
+        console.log()
+        for(let i=0;i<3;i++){
+            if((gamestatus.board[0][i]==gamestatus.board[1][i] && gamestatus.board[1][i]== gamestatus.board[2][i]) && gamestatus.board[0][i] !=null  && gamestatus.board[1][i] !=null && gamestatus.board[2][i] !=null){
+                return true
+            }
+        }
+      
+         if((gamestatus.board[0][0]==gamestatus.board[1][1] &&gamestatus.board[1][1]== gamestatus.board[2][2]) 
+         && gamestatus.board[0][0]!==null && gamestatus.board[1][1]!==null && gamestatus.board[2][2]!==null){
+            return true
+         }
+    
+         if((gamestatus.board[0][2]==gamestatus.board[1][1] &&gamestatus.board[1][1]== gamestatus.board[2][0]) && gamestatus.board[0][2]!==null && gamestatus.board[1][1]!==null && gamestatus.board[2][0]!==null){
+
+            return true
+         }
+
+         return false
+       }
+
+       if(checkwin()){
+        let newmatrix=[]
+        for(let i=0;i<3;i++){
+            let arr=new Array(3).fill(null)
+            newmatrix.push(arr)
+        } 
+        console.log(newmatrix)
+        gamestatus.winnerdata.push(data[1])
+        gamestatus.win=data[1]
+        gamestatus.board=newmatrix
+        io.emit("game",gamestatus)
+       }
+       else if(checkdraw()){
+        let newmatrix=[]
+        for(let i=0;i<3;i++){
+            let arr=new Array(3).fill(null)
+            newmatrix.push(arr)
+        } 
+        gamestatus.winnerdata.push("draw")
+        gamestatus.win="draw"
+        gamestatus.board=newmatrix
+        io.emit("game",gamestatus)
+       }
+       else{
+        io.emit("game",gamestatus)
+       }
+       console.log(gamestatus,"====")
+        
+      
+    })
+
+    socket.on("newround",(data)=>{
+        gamestatus.win=data.win
+        // io.emit("game",gamestatus)
+    })
+    // console.log(gamestatus,"/////////")
+
+     chat(socket,io)
+
+})
+
+
+
+
+
+server.listen(8500,async()=>{
+    try{
+       console.log("server is running on 8500")
+    }
+    catch(err){
+        console.log(err.message)
+     
+    }
+})
